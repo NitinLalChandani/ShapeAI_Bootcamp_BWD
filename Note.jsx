0ff9aae364289c98e18a49cd53432f37825a62c9
NitinLalChandani <68174898+NitinLalChandani@users.noreply.github.com>
2021-06-06 10:50:40 +0530

    Update Note.jsx

--- Note.jsx
+++ Note.jsx
@@ -5,7 +5,7 @@ function Note() {
     <div className="note">
       <h1>Javascript and React.js</h1>
       <p>
-        I have attended the 7 days free bootcamp on Web development with Javascript & React.js conducted by Shape AI
+        I am Nitin Lal Chandani and I have attended the 7 days free bootcamp on Web development with Javascript & React.js conducted by Shape AI
       </p>
     </div>
   );
